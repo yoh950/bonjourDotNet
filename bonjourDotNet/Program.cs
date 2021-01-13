29ef1659ae2eefe0f9ce91faf5e54a256acd66ef
yoh950 <yohann.surville@gmail.com>
2021-01-13 08:53:48 +0100

    first

--- bonjourDotNet/Program.cs
+++ bonjourDotNet/Program.cs
@@ -4,9 +4,32 @@ namespace bonjourDotNet
 {
     class MainClass
     {
+        private const int V = 5;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenue dans le manoir de la peur");
+            Console.WriteLine("Vous arrivé devant le manoir");
+            Console.WriteLine("Voulez vous allez a l'EST ou a 'OUEST?");
+            string response = Console.ReadLine();
+            if(response == "EST")
+            {
+                Console.WriteLine("Vous etes tomber dans un ravin !! pas de chance");
+            }
+            else if(response == "OUEST")
+            {
+                Console.WriteLine("Vous avez trouver deux entrer en face de vous");
+                Console.WriteLine("La fenetre ou la porte ??");
+            }
+            else
+            {
+                while (response != "EST")
+                {
+                    Console.WriteLine("EST ou OUEST ??");
+                    response = Console.ReadLine();
+                }
+            }
+           
         }
     }
 }
